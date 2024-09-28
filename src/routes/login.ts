c939e39cef8c91a4fb143e6d125a7226cabd8437
= <=>
2024-09-28 10:19:49 +0200

    Removed the login ts from testing the generator

--- src/routes/login.ts
+++
@@ -1,9 +0,0 @@
-import { Hono, Context as HonoContext } from "hono";
-
-const app = new Hono();
-
-app.get(`/login`, (c: Hono<Context>) =>
-  c.json("Why did the C++ programmer get lost? He took the wrong branch.")
-);
-
-export default app;
